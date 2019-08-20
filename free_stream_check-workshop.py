cd34d7444b9d452d4c2a12ea5f4cccb535960f50
Jerry Wang <jerryw@jerryw.org>
2019-08-20 09:23:29 +0800

    Update version 2 fix class name in phase 2

--- free_stream_check-workshop.py
+++ free_stream_check-workshop.py
@@ -20,7 +20,7 @@ except Exception:
     pass
 #assert “Online Shopping” in browser.title
 search = browser.find_element_by_id("stream") # Find the search box by class
-search.send_keys("https://broadcast-ettoday.cdn.hinet.net/ettoday-broadcast/smil:etmalltest.smil/playlist.m3u8 ") #send the value of search field
+search.send_keys("https://broadcast-ettoday.cdn.hinet.net/ettoday-broadcast/smil:workshop.smil/playlist.m3u8 ") #send the value of search field
 submit_button = browser.find_element_by_name("commit")
 submit_button.click()
 #Lets wait for lobby page to load
@@ -29,7 +29,7 @@ myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.
 el=browser.find_element_by_tag_name('body')
 time.sleep(15)
 now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-el.screenshot('screencapture'+now+'.png')
+el.screenshot('screencapture-workshop'+now+'.png')
 #browser.save_screenshot("loggedIn.png") ## This will be stored in the location/path mentioned or the current directory
 #myElem.save_screenshot("loggedIn.png") ## This will be stored in the location/path mentioned or the current directory
 try: 
